4ac613c286caf2551f10462b81b3904e9c41c4f2
Bruno Menezes <brunodmenezes@gmail.com>
2013-03-04 04:04:42 -0300

    escalonador concluido dentro das regras aplicadas aging e preempção

--- escalonador.c
+++ escalonador.c
@@ -1,16 +1,13 @@
-/*
-	execute o programa passando os 3 argumentos necessários;
-	./programa <nome_arquivo_leitura> <tamanho_arquivo> <quantidade_io_cpu_burst
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ON  1;
+#define OFF 0;
 
 typedef struct Process Process;
 typedef struct list List;
-typedef struct listObj Obj;
-typedef struct scheduler Scheduler;
+typedef struct obj Obj;
+typedef struct processor Processor;
 typedef struct cpuBurst CpuBurst;
 typedef struct ioBurst IOBurst;
 typedef struct schedulerInfo SchedulerInfo;
@@ -30,11 +27,11 @@ struct Process
 {
 		char name[10];
 		int priority, admission, id;
-		CpuBurst *cpu_burst_init;
-		IOBurst *io_burst_init;
+		CpuBurst *cpuBursts;
+		IOBurst *ioBursts;
 };
 
-struct listObj {
+struct obj {
 	Process *process;
 	Obj *next;
 	Obj *prev;
@@ -45,22 +42,39 @@ struct list {
 };
 
 struct schedulerInfo{
-	int io_done, cpu_done;
+	int processId;
+	CpuBurst *cpu_done;
+	IOBurst *io_done;
 	char status;
 
 };
 
-struct scheduler
+struct processor
 {
-	SchedulerInfo info;
-	int processId;
+	Process *executingProcess;
+	int currentCpuBurstOver;
+	int idle;	
 };
 
 
 void setProcesses(FILE *file, Process*);
 void setProcess(Process *, char *ch, int attrIndex, int);
 char *setInput(char *buffer,int in_tam);
-void schedulerStart(Process *);
+void schedulerStart(Process *, int);
+int hasProcess(List *, List *, List *, Processor *);
+void setNewQueue(List *, Process *, int);
+void addOrderedByAdmission(List *, Obj *);
+void admitProcess(List *, List *, int );
+void scheduleProcess(Processor *, List*, List*);
+void addOrderedByPriority(List *, Obj *);
+void addToQueue(List *, Obj *);
+void aging(List *);
+void preemptionCheck(Processor *processor, List *readyQueue);
+void preemptionExec(Processor *processor, List *readyQueue);
+void execProcess(Processor *);
+void execIO(List *waitQueue);
+void checkIOFinished(List *waitQueue,List *readyQueue);
+void displayQueueStatus(List * , List *);
 
 
 int main(int argc, char **argv){	
@@ -69,39 +83,9 @@ int main(int argc, char **argv){
 	processes = (Process *) malloc(sizeof(Process) * SIZE);
 	FILE *file;
 	file = fopen(argv[1], "r");
-
 	setProcesses(file, processes);
-	//schedulerStart(processes);
-
-
-	/*
-		implementar codigo de escalonamento com preempção e aging
-	*/
-
-	// codigo abaixo só para mostrar que está lendo direito o arquivo de teste
-	for (i = 0; i < SIZE; ++i)
-	{
-		printf("process Id: %d \n",processes[i].id);
-		printf("process name:%s\n",processes[i].name );
-		printf("priority:%d\n",processes[i].priority );
-		printf("process admission : %d\n",processes[i].admission );
-		IOBurst *io;
-		CpuBurst *cpu;
-		cpu = processes[i].cpu_burst_init;
-		io = processes[i].io_burst_init;
-		while(cpu || io){
-			if(cpu){
-				printf("cpu: %d \n", cpu->burstTime);
-				cpu = cpu->next;	
-			}
-			if(io){
-				printf("io:%d\n",io->burstTime);
-				io = io->next;
-			}
-				
-		}
-		printf("#################################################\n");
-	}
+	fclose(file);
+	schedulerStart(processes, SIZE);
 	free(processes);
 
 	return 0;
@@ -140,8 +124,6 @@ void setProcess(Process* p, char *ch, int attrIndex, int processIndex) {
 		case 0:
 			p->id = (processIndex+1);
 			p->admission = atoi(ch);
-			p->cpu_burst_init = NULL;
-			p->io_burst_init = NULL;
 			break;
 		case 1: 
 			strcpy(p->name, ch);
@@ -155,26 +137,23 @@ void setProcess(Process* p, char *ch, int attrIndex, int processIndex) {
 				obj = (CpuBurst *) malloc(sizeof(CpuBurst));
 				obj->burstTime = atoi(ch);
 				obj->next = NULL;
-				if(p->cpu_burst_init == NULL){
-					p->cpu_burst_init = obj;
+				if(p->cpuBursts == NULL){
+					p->cpuBursts = obj;
 				} else {
-					aux = p->cpu_burst_init;
+					aux = p->cpuBursts;
 					while(aux->next != NULL)
 						aux = aux->next;
 					aux->next = obj;
 				}
-				//p->cpuBurst[p->cpuIndex] = 
-				//p->cpuIndex += 1;
-
 			} else {
 				IOBurst *obj, *aux;
 				obj = (IOBurst *) malloc(sizeof(IOBurst));
 				obj->burstTime = atoi(ch);
 				obj->next = NULL;
-				if(p->io_burst_init == NULL){
-					p->io_burst_init = obj;
+				if(p->ioBursts == NULL){
+					p->ioBursts = obj;
 				} else {
-					aux = p->io_burst_init;
+					aux = p->ioBursts;
 					while(aux->next != NULL)
 						aux = aux->next;
 					aux->next = obj;
@@ -196,12 +175,275 @@ char *setInput(char *buffer,int in_tam)
 
 }
 
-void schedulerStart(Process *processes){
-	List *newQueue;
-	List *readyQueue;
-	List *waitQueue;
-	Scheduler scheduler;
+void schedulerStart(Process *processes, int size){
+	List *newQueue, *readyQueue, *waitQueue;
+	Processor *processor;
+	newQueue = (List*) calloc(1,sizeof(List));
+	readyQueue = (List *) calloc(1,sizeof(List));
+	waitQueue = (List *) calloc(1, sizeof(List));
+	processor = (Processor*) calloc(1,sizeof(Processor));
+	processor->idle = ON; //iniciando processador com idle on
+	processor->currentCpuBurstOver = OFF;
+	int adTime, agingTime = 10, currentTime = 0;
+	setNewQueue(newQueue, processes, size);
+	while(hasProcess(newQueue,readyQueue,waitQueue, processor)){
+		admitProcess(newQueue,readyQueue, currentTime);
+		scheduleProcess(processor, readyQueue, waitQueue);
+		execProcess(processor);
+		execIO(waitQueue);
+		checkIOFinished(waitQueue,readyQueue);
+		if( currentTime % agingTime == 0 ){
+			aging(readyQueue);
+		}
+		preemptionCheck(processor,readyQueue);
+		displayQueueStatus(waitQueue, readyQueue);
+		currentTime++;
+	}
+	free(newQueue);
+	free(processor);
+	free(readyQueue);
+	free(waitQueue);
+}
 
+int hasProcess(List *newQueue, List *readyQueue, List *waitQueue, Processor *processor){
+	if(newQueue->first || readyQueue->first || waitQueue->first || processor->executingProcess){
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
+void setNewQueue(List *n, Process *p, int size){
+	int i;
+	for(i=0 ; i<size ; i++){
+		Obj *obj;
+		obj = (Obj*) calloc(1,sizeof(Obj));
+		obj->process = &p[i];
+		addOrderedByAdmission(n,obj);
+	}
+}
+
+void addOrderedByAdmission(List *someQueue, Obj *obj){
+
+	if(someQueue->first == NULL){
+		someQueue->first = obj;
+	}else {
+		Obj *aux;
+		aux = someQueue->first;
+		while(aux->next != NULL && aux->process->admission <= obj->process->admission)
+			aux = aux->next;
+		if(someQueue->first == aux && aux->process->admission > obj->process->admission){
+			obj->next = someQueue->first;
+			someQueue->first = obj;
+			obj->next->prev = obj;
+		} else if(aux->process->admission > obj->process->admission && (aux->next != NULL || aux->next == NULL)){
+			obj->next = aux;
+			aux->prev->next = obj;
+			obj->prev = aux->prev;
+			aux->prev = obj;
+		} else {
+			aux->next = obj;
+			obj->prev = aux;
+		}
+	}
+}
+
+void admitProcess(List *newQueue, List *readyQueue, int currentTime){
+	Obj *obj;
+	obj = newQueue->first;
+	while(obj){
+		if(obj->process->admission == currentTime){
+			newQueue->first = obj->next;
+			newQueue->first ? newQueue->first->prev = NULL : NULL;
+			obj->next = NULL;
+			addOrderedByPriority(readyQueue, obj);
+			obj = newQueue->first;
+		} else {
+			obj = NULL;
+		}
+	}
+}
+
+void addOrderedByPriority(List *someQueue, Obj *obj){
+	Obj *aux;
+	if(someQueue->first == NULL){
+		someQueue->first = obj;
+	}else {
+		aux = someQueue->first;
+		while(aux->next != NULL && aux->process->priority <= obj->process->priority)
+			aux = aux->next;
+		if(someQueue->first == aux && aux->process->priority > obj->process->priority){
+			obj->next = someQueue->first;
+			someQueue->first = obj;
+			obj->next->prev = obj;
+		} else if(aux->process->priority > obj->process->priority && (aux->next != NULL || aux->next == NULL)){
+			obj->next = aux;
+			aux->prev->next = obj;
+			obj->prev = aux->prev;
+			aux->prev = obj;
+		} else {
+			aux->next = obj;
+			obj->prev = aux;
+		}
+	}
+}
+
+void scheduleProcess(Processor *p, List *readyQueue, List *waitQueue){
+	if(p->idle){
+		if(readyQueue->first){
+			Obj *obj;
+			obj = readyQueue->first;
+			readyQueue->first = obj->next;
+			readyQueue->first ? readyQueue->first->prev = NULL : NULL ;
+			obj->next = NULL;
+			p->executingProcess = obj->process;
+			p->idle = OFF;
+		}
+	} else if(p->currentCpuBurstOver){
+		Process *process = p->executingProcess;
+		CpuBurst *cpuBurst = process->cpuBursts;
+		process->cpuBursts = cpuBurst->next;
+		cpuBurst->next = NULL;
+		free(cpuBurst);
+		Obj *obj;
+		obj = (Obj *) calloc(1,sizeof(Obj));
+		obj->process = process;
+		addToQueue(waitQueue, obj);
+		p->currentCpuBurstOver = OFF;
+		p->executingProcess = NULL;
+		p->idle = ON;
+	}
+}
+
+void addToQueue(List *someQueue, Obj *obj){
+	if(!someQueue->first){
+		someQueue->first = obj;
+	}else {
+		Obj *aux;
+		aux = someQueue->first;
+		while(aux->next)
+			aux = aux->next;
+		aux->next = obj;
+		obj->prev = aux;
+	}
+}
+
+void execProcess(Processor *processor){
+	if(processor->executingProcess && !processor->currentCpuBurstOver){
+		printf("\nprocesso %d em execução tempo de CPU %d\n\n",processor->executingProcess->id, processor->executingProcess->cpuBursts->burstTime);
+		if (processor->executingProcess->cpuBursts->burstTime <= 0){
+			processor->currentCpuBurstOver = ON;
+		} else {
+			processor->executingProcess->cpuBursts->burstTime -= 1;
+		}
+	}
+}
+
+void aging(List *readyQueue){
+	if(readyQueue->first){
+		Obj *aux;
+		aux = readyQueue->first;
+		while(aux != NULL){
+			if(aux->process->priority > 0 ) {
+				aux->process->priority -= 1;
+			}
+			aux = aux->next;
+		}
+	}
+}
+
+void preemptionCheck(Processor *processor, List *readyQueue){
+	if (readyQueue->first)
+	{
+		if (processor->executingProcess && processor->executingProcess->priority > readyQueue->first->process->priority)
+		{
+			preemptionExec(processor, readyQueue);
+		}
+	}
+}
+
+void preemptionExec(Processor *processor, List *readyQueue){
+	Process *processOut;
+	Obj *obj;
+	obj = readyQueue->first;
+	if(obj != NULL){
+		readyQueue->first = obj->next;
+		readyQueue->first ? readyQueue->first->prev = NULL : NULL;
+		obj->next = NULL;
+		processOut = processor->executingProcess;
+		processor->executingProcess = obj->process;
+		obj->process = processOut;
+		printf("Preempção: trocando processo %d por processo %d\n",processOut->id, processor->executingProcess->id);
+		addOrderedByPriority(readyQueue,obj);
+	}
+}
+
+void execIO(List *waitQueue){
+	if(waitQueue->first){
+		Obj *aux;
+		aux = waitQueue->first;
+		while(aux != NULL){
+			aux->process->ioBursts->burstTime -= 1;
+			aux = aux->next;
+		}
+	}
+}
+
+void checkIOFinished(List *waitQueue,List *readyQueue){
+	if(waitQueue->first){
+		Obj *obj, *aux;
+		IOBurst *ioBurst;
+		obj = waitQueue->first;
+		while(obj != NULL ){
+			if(waitQueue->first == obj && obj->process->ioBursts->burstTime == 0){
+				waitQueue->first = obj->next;
+				waitQueue->first ? waitQueue->first->prev= NULL : NULL;
+				obj->next = NULL;
+				ioBurst = obj->process->ioBursts;
+				obj->process->ioBursts = ioBurst->next;
+				free(ioBurst);
+				if(obj->process->cpuBursts){
+					addOrderedByPriority(readyQueue, obj);
+				} else {
+					printf("processo %d concluído\n", obj->process->id);
+				}
+				obj = waitQueue->first;
+			} else if(obj->process->ioBursts->burstTime == 0){
+				obj->prev->next = obj->next;
+				obj->next ? obj->next->prev = obj->prev : NULL;
+				aux = obj->next;
+				obj->next = NULL;
+				obj->prev = NULL;
+				ioBurst = obj->process->ioBursts;
+				obj->process->ioBursts = ioBurst->next;
+				free(ioBurst);
+				if(obj->process->cpuBursts){
+					addOrderedByPriority(readyQueue, obj);
+				} else {
+					printf("processo %d concluído\n", obj->process->id);
+				}
+				obj = aux;
+			} else {
+				obj = obj->next;
+			}
+		}
+	}
+}
+
+void displayQueueStatus(List *waitQueue, List *readyQueue){
+	Obj *waitObj, *readyObj;
+	waitObj = waitQueue->first;
+	readyObj = readyQueue->first;
+	printf("\nFila de Espera:");
+	while(waitObj != NULL){
+		printf(" Id:%d P:%d |",waitObj->process->id, waitObj->process->priority);
+		waitObj = waitObj->next;
+	}
+	printf("\nFila de Prontos:");
+	while(readyObj != NULL){
+		printf(" Id:%d Prioridade:%d |",readyObj->process->id, readyObj->process->priority);
+		readyObj = readyObj->next;
+	}
+	printf("\n");
 }
 
